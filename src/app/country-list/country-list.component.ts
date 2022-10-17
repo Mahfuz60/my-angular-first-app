5dd0dc679db660de6df90fc11c6d84c771f77a69
Mahfuz Alam <36205825+Mahfuz60@users.noreply.github.com>
2022-10-17 10:20:04 +0600

    inline-css-template

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
     <h1 class="country">
     Country List
     </h1>
+    <div class="para">
+      <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quasi optio. Fugiat amet sint maiores excepturi consectetur. Minima, placeat ipsam? Quasi recusandae id consequatur aliquam quos vel nihil odit voluptate molestiae autem cum, blanditiis veritatis voluptatum excepturi. Nisi deserunt, esse laboriosam tempore alias tenetur, quibusdam quas est dolor neque totam!</p>
+    </div>
   `,
   styles: [
     `
@@ -14,6 +17,17 @@ import { Component, OnInit } from '@angular/core';
       font-weight:bold;
       margin-top:10px;
     }
+    .para{
+      border:2px solid black;
+      border-radius:40px;
+      padding:20px;
+      background-color:#9b32a8;
+       }
+      .para p{
+        font-weight:500;
+        font-style:italic;
+        color:#C6C8C6; 
+       }
     
     `
   ]
