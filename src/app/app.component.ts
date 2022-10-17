7ac0a743a48130b2c008b9c335ae7dbe35d1d5dd
Mahfuz Alam <36205825+Mahfuz60@users.noreply.github.com>
2022-10-17 13:29:19 +0600

    angular functio called button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = ' Angular App';
   data="Code step by learning from Angular";
+  
 
   // function data
   getValue(){
     return "Hello Bangladesh,What day will going on!"
+  };
+  getName=()=>{
+    alert("Hello function you are call me!")
   }
 }
